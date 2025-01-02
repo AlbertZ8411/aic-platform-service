9146273a3320417e219b0004c763ef0ea7857552
Zhu Albert <zhualbert@Zhus-MBP.lan>
2025-01-02 12:25:47 +0800

    added user and account relative creatation sql

---
+++ src/main/resources/db/migration/V1.1.0__aic_db_user.sql
@@ -0,0 +1,43 @@
+CREATE TABLE IF NOT EXISTS t_user(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    b_id char(36) NOT NULL  UNIQUE,
+    username varchar(30),
+    first_name varchar(20),
+    last_name varchar(20),
+    fk_creator char(36) NOT NULL,
+    create_date datetime NOT NULL,
+    update_time datetime,
+    fk_updater char(36),
+    phone_no varchar(15),
+    email varchar(100),
+    code varchar(10),
+    fk_avartar_b_id char(36)
+);
+
+CREATE TABLE IF NOT EXISTS t_account_inf(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    fk_user_b_id char(36) NOT NULL,
+    password varchar(20),
+    fk_auth_status int NOT NULL,
+    fk_auth_type int NOT NULL,
+    auth_id varchar(100)
+);
+
+CREATE TABLE IF NOT EXISTS t_auth_type(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(20)
+);
+
+CREATE TABLE IF NOT EXISTS t_auth_status(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(20)
+);
+
+CREATE TABLE IF NOT EXISTS t_auth_third_party(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    fk_user_b_id char(36),
+    open_id char(100),
+    access_token char(100),
+    access_expired timestamp,
+    fk_auth_type int
+);
