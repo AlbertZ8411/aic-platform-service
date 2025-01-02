e5b9ad191e3b6d19d59adc0b19ff047fe7d991ba
Zhu Albert <zhualbert@Zhus-MBP.lan>
2025-01-02 12:52:16 +0800

    added condition classes

---
+++ src/main/java/com/tencent/wxcloudrun/model/common/Condition.java
@@ -0,0 +1,17 @@
+package com.tencent.wxcloudrun.model.common;
+
+import lombok.Data;
+
+
+/**
+ *
+ * @author <a href="mailto:albert.zhu8411@gmail.com">Albert Zhu</a>
+ * @version 1.0
+ *
+ * */
+@Data
+public class Condition<T> {
+
+    private T instance;
+
+}
